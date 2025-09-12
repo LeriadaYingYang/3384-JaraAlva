330ce8f01d6ab39a1c63bbfcb238169804fb492f
Jara Alva Daniel <Jade@upn.pe>
2025-09-11 19:40:29 -0500

    Completando el Ejemplo

--- Ejercicio Pdf/Semana3/Practicamos/Python/Ejercicio_Practicamos_1.py
+++ Ejercicio Pdf/Semana3/Practicamos/Python/Ejercicio_Practicamos_1.py
@@ -1,2 +1,6 @@
 # Hallar la hipotenusa de un triángulo rectángulo.
 a = float(input("Ingrese el valor del cateto a: "))
+b = float(input("Ingrese el valor del cateto b: "))
+c = (a**2 + b**2)**0.5
+print("El valor de la hipotenusa es:", c)
+
