06524367578e8b88ec41fb57c29f97733cfbc913
Jara Alva Daniel <Jade@upn.pe>
2025-09-11 20:25:53 -0500

    Agregando el Ejercicio 3 en python

---
+++ Ejercicio Pdf/Semana3/Practicamos/Python/Ejercicio_Practicamos_3.py
@@ -0,0 +1,11 @@
+# Dar la suma, el producto y el promedio de tres números.
+float1 = float(input("Ingrese el primer número: "))
+float2 = float(input("Ingrese el segundo número: "))
+float3 = float(input("Ingrese el tercer número: "))
+suma = float1 + float2 + float3
+producto = float1 * float2 * float3
+promedio = suma / 3
+print("La suma es:", suma)
+print("El producto es:", producto)
+print("El promedio es:", promedio)
+
