79bc8eb820d1fb794767e0d73d0ce7f67f2dff8e
Jara Alva Daniel <Jade@upn.pe>
2025-09-11 20:21:42 -0500

    Agregando el ejercicio 2

---
+++ Ejercicio Pdf/Semana3/Practicamos/Python/Ejercicio_Practicamos_2.py
@@ -0,0 +1,8 @@
+# Hallar el promedio ponderado, si nota 1(20%), nota 2(30%), nota 3(40%) y nota 4(10%).
+nota1 = float(input("Ingrese la nota 1 (20%): "))
+nota2 = float(input("Ingrese la nota 2 (30%): "))
+nota3 = float(input("Ingrese la nota 3 (40%): "))
+nota4 = float(input("Ingrese la nota 4 (10%): "))
+promedio_ponderado = (nota1 * 0.2) + (nota2 * 0.3) + (nota3 * 0.4) + (nota4 * 0.1)
+print("El promedio ponderado es:", promedio_ponderado)
+
