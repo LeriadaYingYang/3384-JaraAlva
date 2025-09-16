4d373af5f168f8a885764e2fcebc8129efd9b9f6
Jara Alva Daniel <Jade@upn.pe>
2025-09-16 17:03:55 -0500

    Problema

--- EjercicioClaseSemana5/Ejercicio en C#_Anidado/Program.cs
+++ EjercicioClaseSemana5/Ejercicio en C#_Anidado/Program.cs
@@ -10,6 +10,12 @@ namespace Ejercicio_en_C__Anidado
     {
         static void Main(string[] args)
         {
+            /*En el proceso de admisión de una universidad pública, el costo de inscripción es de 240 soles, en la tabla se muestra el monto que deberá ser agregado a la inscripción.
+Si un postulante es hijo de un trabajador de la universidad no paga inscripción.
+Elaborar un programa que pida el tipo de colegio, lugar de nacimiento y si es hijo de trabajador universitario; luego de ello calcular el monto a pagar.
+*/
+
+
         }
     }
 }
