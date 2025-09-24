f23611bb858a0045a02f89e8b0deda13f241c5fa
Jara Alva Daniel <Jade@upn.pe>
2025-09-24 11:24:13 -0500

    Agregando el segundo Ejemplo de Secuencial

--- 02_EstructurasControl/Secuenciales/CSharp/Ejemplo2/Program.cs
+++ 02_EstructurasControl/Secuenciales/CSharp/Ejemplo2/Program.cs
@@ -10,6 +10,15 @@ namespace Ejemplo2
     {
         static void Main(string[] args)
         {
+            /*Conversión de temperaturas*/
+            Console.Write("Ingrese temperatura en Celsius: ");
+            double celsius = double.Parse(Console.ReadLine());
+
+            double fahrenheit = (celsius * 9 / 5) + 32;
+            double kelvin = celsius + 273.15;
+
+            Console.WriteLine("En Fahrenheit: " + fahrenheit);
+            Console.WriteLine("En Kelvin: " + kelvin);
         }
     }
 }
---
+++ 02_EstructurasControl/Secuenciales/Python/Ejemplo2.py
@@ -0,0 +1,9 @@
+# Conversión de temperaturas
+
+celsius = float(input("Ingrese temperatura en Celsius: "))
+
+fahrenheit = (celsius * 9 / 5) + 32
+kelvin = celsius + 273.15
+
+print("En Fahrenheit:", fahrenheit)
+print("En Kelvin:", kelvin)
