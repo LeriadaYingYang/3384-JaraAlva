8c0c5e4f9f87a21209004c92e633d85cf584bd96
Jara Alva Daniel <Jade@upn.pe>
2025-09-24 21:11:02 -0500

    Actualizando el ejercicio 2 de Dobles

---
+++ 02_EstructurasControl/Condicionales/Dobles/CSharp/Ejemplo02/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejemplo02
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Un programa que pida un número y determine si es par o impar.*/
+
+            Console.Write("Ingrese un número: ");
+            int numero = int.Parse(Console.ReadLine());
+
+            if (numero % 2 == 0)
+            {
+                Console.WriteLine("El número es PAR");
+            }
+            else
+            {
+                Console.WriteLine("El número es IMPAR");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ 02_EstructurasControl/Condicionales/Dobles/Python/Ejemplo2.py
@@ -0,0 +1,7 @@
+# Un programa que pida un número y determine si es par o impar.
+numero = int(input("Ingrese un número: "))
+
+if numero % 2 == 0:
+    print("El número es PAR")
+else:
+    print("El número es IMPAR")
