ddc1bb0c2b19bb390d5d0649e2807b6df2796ebe
Jara Alva Daniel <Jade@upn.pe>
2025-10-02 11:58:12 -0500

    ...

---
+++ 02_EstructurasControl/Repetitivas/Semana7_DoWhile/Practicamos/CSharpp/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n, digito;
+
+            Console.Write("Ingrese un número entero: ");
+            n = int.Parse(Console.ReadLine());
+
+            Console.Write("Número invertido: ");
+
+            do
+            {
+                digito = n % 10;         
+                Console.Write(digito);  
+                n = n / 10;   
+            }
+            while (n > 0);
+
+            Console.WriteLine();
+        }
+    }
+}
