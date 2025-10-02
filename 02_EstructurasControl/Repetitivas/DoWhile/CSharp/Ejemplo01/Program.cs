9c8b116a770820cc8ac436b30092ab66df01cd87
Jara Alva Daniel <Jade@upn.pe>
2025-10-02 11:51:20 -0500

    Enviando los dos ejercicos actualziados

--- 02_EstructurasControl/Repetitivas/DoWhile/CSharp/Ejemplo01/Program.cs
+++ 02_EstructurasControl/Repetitivas/DoWhile/CSharp/Ejemplo01/Program.cs
@@ -10,7 +10,22 @@ namespace Ejemplo01
     {
         static void Main(string[] args)
         {
-            /*1*/
+            int n, digito;
+
+            Console.Write("Ingrese un número entero: ");
+            n = int.Parse(Console.ReadLine());
+
+            Console.Write("Número invertido: ");
+
+            do
+            {
+                digito = n % 10;
+                Console.Write(digito);
+                n = n / 10;
+            }
+            while (n > 0);
+
+            Console.WriteLine();
         }
     }
 }
